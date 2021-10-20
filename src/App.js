f7d57540d89ff8f09bde2a5f3ce0ab21ba697003
Raoul C. Covaci <raoul.c@webwavers.net>
2021-10-20 12:30:52 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Cat from './Cat';
 
 function App() {
   return (
@@ -15,9 +16,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hakuna matata
         </a>
       </header>
+      <Cat />
     </div>
   );
 }
---
+++ src/Cat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Cat.css';
+
+function Cat() {
+ return (
+     <React.Fragment>
+        <h1 className="Cat">this is my bussiness cat</h1>
+     </React.Fragment>
+ );
+}
+
+export default Cat;
