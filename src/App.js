04507057ff069fd0c7019e5aea07c351319e7c72
Raoul C. Covaci <raoul.c@webwavers.net>
2021-10-21 17:58:45 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -22,44 +22,46 @@ function App() {
         formFor={types} 
         formId={types}
         formName={types}
-        title={'Registrer'}
+        title="Registrer"
         register={true} 
-        buttonTheme={'button'}
-        buttonType={'submit'}
-        buttonLabel={'Register'}
+        buttonTheme="button"
+        buttonType="submit"
+        buttonLabel="Register"
         />
         <UserForm formType={name} 
         formFor={types} 
         formId={types} 
         formName={types} 
-        title={'Edit Profile'} 
+        title="Edit Profile" 
         register={false} 
-        buttonTheme={'button'}
-        buttonType={'submit'}
-        buttonLabel={'Update'}
+        buttonTheme="button"
+        buttonType="submit"
+        buttonLabel="Update"
         /> */}
 
         <UserForm
-        title={'Register'}       
-        buttonTheme={'button'}
-        buttonType={'submit'}
-        buttonLabel={'Register'}>
-          <FormControl type={'Username'} for={'text'} name={'username'} id={'username'} />
-          <FormControl type={'Email'} for={'email'} name={'email'} id={'email'} />
-          <FormControl type={'Password'} for={'password'} name={'password'} id={'password'} />
-          <FormControl type={'Confirm Password'} for={'password'} name={'confirmPassword'} id={'confirmPassword'} />
-          <FormControl type={'Motto'} for={'textarea'} name={'motto'} id={'motto'} />
-          <FormControl type={'User personalized'} for={'radio'} name={'userPersonalized'} id={'userPersonalized'} />
+          title="Register"
+          buttonTheme="button"
+          buttonType="submit"
+          buttonLabel="Register"
+        >
+          <FormControl type="text" label="Username" for="text" name="username" id="username" />
+          <FormControl type="email" label="Email" for="email" name="email" id="email" />
+          <FormControl type="password" label="Password" for="password" name="password" id="password" />
+          <FormControl type="password" label="Confirm Password" for="password" name="confirmPassword" id="confirmPassword" />
+          <FormControl type="textarea" label="Motto" for="textarea" name="motto" id="motto" />
+          <FormControl type="radio" label="User personalized" for="radio" name="userPersonalized" id="userPersonalized" />
         </UserForm>
         <UserForm       
-        title={'Edit Profile'}  
-        buttonTheme={'button'}
-        buttonType={'submit'}
-        buttonLabel={'Update'}>
-          <FormControl type={'Username'} for={'text'} name={'username'} id={'username'} value={TheProfile.username}/>
-          <FormControl type={'Email'} for={'email'} name={'email'} id={'email'} value={TheProfile.email}/>
-          <FormControl type={'Motto'} for={'textarea'} name={'motto'} id={'motto'} value={TheProfile.motto}/>
-          <FormControl type={'User personalized'} for={'radio'} name={'userPersonalized'} id={'userPersonalized'} isChecked={true}/>
+          title="Edit Profile"  
+          buttonTheme="button"
+          buttonType="submit"
+          buttonLabel="Update"
+        >
+          <FormControl label="Username" for="text" name="username" id="username" value={TheProfile.username}/>
+          <FormControl label="Email" for="email" name="email" id="email" value={TheProfile.email}/>
+          <FormControl label="Motto" for="textarea" name="motto" id="motto" value={TheProfile.motto}/>
+          <FormControl label="User personalized" for="radio" name="userPersonalized" id="userPersonalized" isChecked={true}/>
         </UserForm>
       </div>
 
--- src/FormControl/FormControl.js
+++ src/FormControl/FormControl.js
@@ -3,16 +3,16 @@ import './FormControl.css';
 
 function FormControl(props) {
  return (
-  <div className='form-control'>
-    <label className={props.type}>{props.type}</label>
+  <div className="form-control">
+    <label>{props.label}</label>
     <br/>
-    {props.for == 'textarea' ? 
+    {props.for == "textarea" ? 
       <textarea htmlFor={props.for} name={props.name} id={props.id} value={props.value}></textarea>
     :
-      props.for == 'radio' ?
+      props.for == "radio" ?
        <React.Fragment>
-         <input type={props.for} htmlFor={props.for} name={props.name} id={props.id} value={true} checked={props.isChecked == true ? props.isChecked : ''} /> yes
-         <input type={props.for} htmlFor={props.for} name={props.name} id={props.id} value={false} checked={props.isChecked == false ? props.isChecked  : ''}/> no
+         <input type={props.type} htmlFor={props.for} name={props.name} id={props.id} value={true} checked={props.isChecked == true ? props.isChecked : ''} /> yes
+         <input type={props.type} htmlFor={props.for} name={props.name} id={props.id} value={false} checked={props.isChecked == false ? props.isChecked  : ''}/> no
        </React.Fragment>
       :
         <input htmlFor={props.for} name={props.name} value={props.value} id={props.id} />
